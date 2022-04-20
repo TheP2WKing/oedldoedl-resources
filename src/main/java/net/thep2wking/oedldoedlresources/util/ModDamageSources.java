bec2dfb82d188377de9de4494f5500c4f6dbc1fa
TheP2WKing <78441179+TheP2WKing@users.noreply.github.com>
2022-04-20 22:26:22 +0200

    Delete src/main/java/net/thep2wking/oedldoedlresources directory

--- src/main/java/net/thep2wking/oedldoedlresources/util/ModDamageSources.java
+++
@@ -1,8 +0,0 @@
-package net.thep2wking.oedldoedlresources.util;
-
-import net.minecraft.util.DamageSource;
-
-public class ModDamageSources 
-{
-	public static final DamageSource UNSTABLE = (new DamageSource("unstable").setDamageBypassesArmor());
-}
